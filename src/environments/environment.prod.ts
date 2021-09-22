ee0bb4b4e5cfb6ef60e624712e9dc7bc52679e20
AleVzT <agarcialg66@gmail.com>
2021-09-22 17:41:44 -0300

    actualizando env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
     production: true,
-    /* firebase: {
-        apiKey: "AIzaSyAHoeOGk-ZzJQ-d7CtEJToE3qt2Vj2MIWc",
-        authDomain: "firestore-shopping-9f5f3.firebaseapp.com",
-        projectId: "firestore-shopping-9f5f3",
-        storageBucket: "firestore-shopping-9f5f3.appspot.com",
-        messagingSenderId: "753109648328",
-        appId: "1:753109648328:web:c3f5eca8f8872c23b8f6fd"
-    } */
 };
